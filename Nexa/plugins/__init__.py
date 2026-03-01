9adfe5ffd103c7595cb388b8ce64993bb786b2c3
Ivan <nexacoders2@gmail.com>
2026-03-01 16:29:52 +0530

    Upload Nexa/plugins/__init__.py

---
+++ Nexa/plugins/__init__.py
@@ -0,0 +1,28 @@
+# This file ensures all plugins are loaded when the package is imported
+
+# Import each plugin manually
+from Nexa.plugins import test
+
+from Nexa.plugins import start
+
+from Nexa.plugins import view_accounts
+
+from Nexa.plugins import set_msg
+
+from Nexa.plugins import host_account
+
+from Nexa.plugins import start_broadcast
+
+from Nexa.plugins import stop_broadcast
+
+from Nexa.plugins import analytics
+
+from Nexa.plugins import detailed_report
+
+from Nexa.plugins import autoreply
+
+from Nexa.plugins import set_delay
+
+from Nexa.plugins import delete_accounts
+
+from Nexa.plugins import mustjoin
