230c92123c96ebdc3c592a41d00940d489972313
Ivan <nexacoders2@gmail.com>
2026-03-30 15:46:30 +0530

    Update __init__.py

--- Nexa/__init__.py
+++ Nexa/__init__.py
@@ -1,10 +1,7 @@
-# Nexa/__init__.py
-
-# DATABASE
 from .database.mongo import db
 from .database import users
 
-# CORE
+
 from .core import broadcast_engine
 from .core import session_manager
 from .core import task_manager
