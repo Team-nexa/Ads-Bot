d41b3208e010503b7193c4c8ae9ce3ce604e5442
Ivan <nexacoders2@gmail.com>
2026-03-30 15:45:52 +0530

    Delete test.py

--- Nexa/plugins/test.py
+++
@@ -1,6 +0,0 @@
-from Nexa.bot import bot
-from pyrogram import filters
-
-@bot.on_message(filters.command("test"))
-async def test_command(client, message):
-    await message.reply_text("✅ Plugin is working!")
